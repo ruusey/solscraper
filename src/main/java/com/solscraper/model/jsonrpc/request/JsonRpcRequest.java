4f906f648ce62eab163baf09278eee955f5df64c
Robert Usey <ruusey@gmail.com>
2024-10-28 09:45:37 -0400

    Format

--- src/main/java/com/solscraper/model/jsonrpc/request/JsonRpcRequest.java
+++ src/main/java/com/solscraper/model/jsonrpc/request/JsonRpcRequest.java
@@ -19,57 +19,46 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class JsonRpcRequest {
-	private static final String RAYDIUM_TOKEN_POOL = "7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5";
-	@JsonProperty("method")
-	private String method;
-	@JsonProperty("jsonrpc")
-	private String jsonrpc;
-	@JsonProperty("params")
-	private List<Object> params;
-	@JsonProperty("id")
-	private String id;
+    private static final String RAYDIUM_TOKEN_POOL = "7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5";
+    @JsonProperty("method")
+    private String method;
+    @JsonProperty("jsonrpc")
+    private String jsonrpc;
+    @JsonProperty("params")
+    private List<Object> params;
+    @JsonProperty("id")
+    private String id;
 
-	
-	public static JsonRpcRequest getSignatureRequest() {
-		final Map<String, Object> extraParams = new HashMap<String, Object>();
-		extraParams.put("limit", 10);
+    public static JsonRpcRequest getSignatureRequest() {
+        final Map<String, Object> extraParams = new HashMap<String, Object>();
+        extraParams.put("limit", 10);
 
-		return JsonRpcRequest.builder().method("getSignaturesForAddress")
-				.jsonrpc("2.0")
-				.params(Arrays.asList(RAYDIUM_TOKEN_POOL, extraParams))
-				.id(UUID.randomUUID().toString())
-				.build();
-	}
-	
-	       public static JsonRpcRequest getSignatureRequest(String addr) {
-	                final Map<String, Object> extraParams = new HashMap<String, Object>();
-	                extraParams.put("limit", 10);
+        return JsonRpcRequest.builder().method("getSignaturesForAddress").jsonrpc("2.0").params(Arrays.asList(RAYDIUM_TOKEN_POOL, extraParams))
+                .id(UUID.randomUUID().toString()).build();
+    }
 
-	                return JsonRpcRequest.builder().method("getSignaturesForAddress")
-	                                .jsonrpc("2.0")
-	                                .params(Arrays.asList(addr, extraParams))
-	                                .id(UUID.randomUUID().toString())
-	                                .build();
-	        }
-	
-	public static JsonRpcRequest getTokenAccountRequest(String mintAddress) {
-		final Map<String, Object> extraParams = new HashMap<String, Object>();
-		extraParams.put("encoding", "jsonParsed");
-		extraParams.put("commitment", "confirmed");
-		return JsonRpcRequest.builder().method("getMultipleAccounts")
-				.jsonrpc("2.0")
-				.params(Arrays.asList(Arrays.asList(mintAddress), extraParams))
-				.id(UUID.randomUUID().toString())
-				.build();
-	}
-	
-	public static JsonRpcRequest getTransactionRequest(String transactionSig) {
-		final Map<String, Object> extraParams = new HashMap<String, Object>();
-		extraParams.put("encoding", "jsonParsed");
-		extraParams.put("commitment", "confirmed");
-		extraParams.put("maxSupportedTransactionVersion", 0);
-		return JsonRpcRequest.builder().method("getTransaction").jsonrpc("2.0")
-				.params(Arrays.asList(transactionSig, extraParams))
-				.id(UUID.randomUUID().toString()).build();
-	}
+    public static JsonRpcRequest getSignatureRequest(String addr) {
+        final Map<String, Object> extraParams = new HashMap<String, Object>();
+        extraParams.put("limit", 10);
+
+        return JsonRpcRequest.builder().method("getSignaturesForAddress").jsonrpc("2.0").params(Arrays.asList(addr, extraParams))
+                .id(UUID.randomUUID().toString()).build();
+    }
+
+    public static JsonRpcRequest getTokenAccountRequest(String mintAddress) {
+        final Map<String, Object> extraParams = new HashMap<String, Object>();
+        extraParams.put("encoding", "jsonParsed");
+        extraParams.put("commitment", "confirmed");
+        return JsonRpcRequest.builder().method("getMultipleAccounts").jsonrpc("2.0").params(Arrays.asList(Arrays.asList(mintAddress), extraParams))
+                .id(UUID.randomUUID().toString()).build();
+    }
+
+    public static JsonRpcRequest getTransactionRequest(String transactionSig) {
+        final Map<String, Object> extraParams = new HashMap<String, Object>();
+        extraParams.put("encoding", "jsonParsed");
+        extraParams.put("commitment", "confirmed");
+        extraParams.put("maxSupportedTransactionVersion", 0);
+        return JsonRpcRequest.builder().method("getTransaction").jsonrpc("2.0").params(Arrays.asList(transactionSig, extraParams))
+                .id(UUID.randomUUID().toString()).build();
+    }
 }
